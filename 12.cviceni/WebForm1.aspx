3371d1588f5771a31c32cf70aa1cb6242abb049c
Šimon Prokop <xproko42@vutbr.cz>
2021-04-29 15:22:00 +0200

    Add project files.

---
+++ 12.cviceni/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_12.cviceni.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="Operand1" runat="server"></asp:TextBox>
+		<asp:DropDownList ID="Operace" runat="server">
+			<asp:ListItem>+</asp:ListItem>
+			<asp:ListItem>-</asp:ListItem>
+			<asp:ListItem>*</asp:ListItem>
+			<asp:ListItem Value="/"></asp:ListItem>
+		</asp:DropDownList>
+		<asp:TextBox ID="Operand2" runat="server"></asp:TextBox>
+		<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Spocti" />
+		<asp:TextBox ID="Vysledek" runat="server"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ 12.cviceni/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _12.cviceni
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Button1_Click(object sender, EventArgs e)
+		{
+			ServiceReference1.Service1Client sluzba = new ServiceReference1.Service1Client();
+
+			if (Operand2.Text == "0" && Operace.SelectedValue == "/")
+			{
+				Vysledek.Text = "Nelze delit nulou!";
+				Response.Redirect("https://i.imgflip.com/2oixw7.jpg");
+
+			}
+			if (Operand1.Text != "" && Operand2.Text != "")
+			{
+				Vysledek.Text = Convert.ToString(sluzba.Spocti(Convert.ToDecimal(Operand1.Text), Convert.ToDecimal(Operand2.Text), Operace.SelectedValue));
+			}
+
+			else
+			{
+				Vysledek.Text = "Chyba na straně programátora :] ";
+			}
+
+
+
+		}
+	}
+}
